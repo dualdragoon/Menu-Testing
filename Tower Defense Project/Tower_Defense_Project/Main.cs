55ea6479d111abb9e646819fd07853ae0edf96b9
dualdragoon <hosleraaron06@gmail.com>
2016-12-10 16:12:53 -0500

    Apparently I lied. Started updating this one simultaneously with the game.

--- Tower Defense Project/Tower_Defense_Project/Main.cs
+++ Tower Defense Project/Tower_Defense_Project/Main.cs
@@ -25,12 +25,23 @@ namespace Tower_Defense_Project
         static MouseManager mouseManager;
         static MouseState mouse;
 
+        string lastMenu = "Main Menu";
         Texture2D background;
+        Vector2 intendedResolution;
 
         List<Button> buttons = new List<Button>();
-        List<string> buttonDestinations = new List<string>();
+        List<ButtonType> buttonTypes = new List<ButtonType>();
+        List<int> buttonLeftResults = new List<int>();
+        List<int> buttonRightResults = new List<int>();
+        List<string> buttonLeftDestinations = new List<string>();
+        List<string> buttonRightDestinations = new List<string>();
         List<Texture2D> textures = new List<Texture2D>();
         List<Vector2> textureLocations = new List<Vector2>();
+
+        public string LastMenu
+        {
+            get { return lastMenu; }
+        }
 
         #region Properties
         public static ContentManager GameContent
@@ -41,6 +52,11 @@ namespace Tower_Defense_Project
         public static GraphicsDeviceManager Graphics
         {
             get { return graphics; }
+        }
+
+        public static Vector2 Scale
+        {
+            get { return new Vector2(Graphics.PreferredBackBufferWidth, Graphics.PreferredBackBufferHeight); }
         }
 
         public static KeyboardManager Keyboard
@@ -113,67 +129,74 @@ namespace Tower_Defense_Project
             // TODO: Unload any non ContentManager content here
         }
 
-        private void ButtonPressed(object sender, EventArgs e)
-        {
-            string name = buttonDestinations[((Button)sender).ButtonNum];
-            background = GameContent.Load<Texture2D>("Textures/help");
-            buttons.Clear();
-            buttonDestinations.Clear();
-            textures.Clear();
-            textureLocations.Clear();
-            LoadMenu(name);
-        }
-
-        private void LoadMenu(string menuName)
+        public void LoadMenu(string menuName)
         {
             try
             {
                 XmlDocument read = new XmlDocument();
                 read.Load("Content/Menus/" + menuName + ".xml");
                 XmlNode node = read.SelectSingleNode("/Menu");
+                XmlNode res = node.SelectSingleNode("IntendedResolution");
+                XmlNode resX = res.SelectSingleNode("X"), resY = res.SelectSingleNode("Y");
+                intendedResolution = new Vector2(float.Parse(resX.InnerText), float.Parse(resY.InnerText));
 
                 foreach (XmlNode i in node)
                 {
-                    if (i.Name == "BackgroundTexture") background = GameContent.Load<Texture2D>("Menus/" + menuName + "/" + i.InnerText + ".png");
+                    if (i.Name == "BackgroundTexture") background = Main.GameContent.Load<Texture2D>("Menus/" + menuName + "/" + i.InnerText + ".png");
                     else if (i.Name == "Texture")
                     {
                         foreach (XmlNode l in i)
                         {
-                            if (l.Name == "TextureName") textures.Add(GameContent.Load<Texture2D>("Menus/" + menuName + "/" + l.InnerText + ".png"));
+                            if (l.Name == "TextureName") textures.Add(Main.GameContent.Load<Texture2D>("Menus/" + menuName + "/" + l.InnerText + ".png"));
                             else if (l.Name == "Position")
                             {
                                 XmlNode x = l.SelectSingleNode("X"), y = l.SelectSingleNode("Y");
-                                textureLocations.Add(new Vector2(float.Parse(x.InnerText), float.Parse(y.InnerText)));
+                                textureLocations.Add(new Vector2((float.Parse(x.InnerText) / intendedResolution.X) * Main.Scale.X, (float.Parse(y.InnerText) / intendedResolution.Y) * Main.Scale.Y));
                             }
                         }
                     }
                     else if (i.Name == "Button")
                     {
                         ButtonType b = ButtonType.Ellipse;
-                        Texture2D normal = GameContent.Load<Texture2D>("Textures/help"), hovered = GameContent.Load<Texture2D>("Textures/help");
+                        Texture2D normal = Main.GameContent.Load<Texture2D>("Textures/help"), hovered = Main.GameContent.Load<Texture2D>("Textures/help");
                         Vector2 pos = Vector2.Zero;
                         float w = 0, h = 0, diameter = 0;
+                        int leftResultType = 0, rightResultType = 0;
                         foreach (XmlNode l in i)
                         {
                             if (l.Name == "ButtonType")
                             {
                                 b = (ButtonType)int.Parse(l.InnerText);
+                                buttonTypes.Add(b);
                             }
-                            else if (l.Name == "NormalTexture") normal = GameContent.Load<Texture2D>("Menus/" + menuName + "/" + l.InnerText + ".png");
-                            else if (l.Name == "HoveredTexture") hovered = GameContent.Load<Texture2D>("Menus/" + menuName + "/" + l.InnerText + ".png");
+                            else if (l.Name == "NormalTexture") normal = Main.GameContent.Load<Texture2D>("Menus/" + menuName + "/" + l.InnerText + ".png");
+                            else if (l.Name == "HoveredTexture") hovered = Main.GameContent.Load<Texture2D>("Menus/" + menuName + "/" + l.InnerText + ".png");
                             else if (l.Name == "Position")
                             {
                                 XmlNode x = l.SelectSingleNode("X"), y = l.SelectSingleNode("Y");
                                 pos = new Vector2(float.Parse(x.InnerText), float.Parse(y.InnerText));
                             }
-                            else if (l.Name == "DestinationMenu") buttonDestinations.Add(l.InnerText);
+                            else if (l.Name == "LeftClickResult")
+                            {
+                                XmlNode type = l.SelectSingleNode("ResultType"), name = l.SelectSingleNode("ResultName");
+                                leftResultType = int.Parse(type.InnerText);
+                                buttonLeftResults.Add(leftResultType);
+                                buttonLeftDestinations.Add(name.InnerText);
+                            }
+                            else if (l.Name == "RightClickResult")
+                            {
+                                XmlNode type = l.SelectSingleNode("ResultType"), name = l.SelectSingleNode("ResultName");
+                                rightResultType = int.Parse(type.InnerText);
+                                buttonRightResults.Add(rightResultType);
+                                buttonRightDestinations.Add(name.InnerText);
+                            }
                             else if (b == ButtonType.Rectangle)
                             {
                                 if (l.Name == "Size")
                                 {
                                     XmlNode width = l.SelectSingleNode("Width"), height = l.SelectSingleNode("Height");
-                                    w = float.Parse(width.InnerText);
-                                    h = float.Parse(height.InnerText);
+                                    w = (float.Parse(width.InnerText) / intendedResolution.X) * Main.Scale.X;
+                                    h = (float.Parse(height.InnerText) / intendedResolution.Y) * Main.Scale.Y;
                                 }
                             }
                             else if (b == ButtonType.Circle)
@@ -181,15 +204,86 @@ namespace Tower_Defense_Project
                                 if (l.Name == "Diameter") diameter = float.Parse(l.InnerText);
                             }
                         }
-                        if (b == ButtonType.Rectangle) buttons.Add(new Button(pos, (int)w, (int)h, buttons.Count, CurrentMouse, normal, hovered, true, 800, 480));
-                        else if (b == ButtonType.Circle) buttons.Add(new Button(pos, diameter, buttons.Count, CurrentMouse, normal, hovered, true, 800, 480));
-                        else if (b == ButtonType.Ellipse) buttons.Add(new Button(pos, buttons.Count, CurrentMouse, normal, hovered, true, 800, 480));
+                        pos = new Vector2((pos.X / intendedResolution.X) * Main.Scale.X, (pos.Y / intendedResolution.Y) * Main.Scale.Y);
+                        if (b == ButtonType.Rectangle) buttons.Add(new Button(pos, (int)w, (int)h, buttons.Count, Main.CurrentMouse, normal, hovered, true, Main.Scale.X, Main.Scale.Y));
+                        else if (b == ButtonType.Circle) buttons.Add(new Button(pos, diameter, buttons.Count, Main.CurrentMouse, normal, hovered, true, Main.Scale.X, Main.Scale.Y));
+                        else if (b == ButtonType.Ellipse) buttons.Add(new Button(pos, buttons.Count, Main.CurrentMouse, normal, hovered, true, Main.Scale.X, Main.Scale.Y));
                     }
                 }
 
                 foreach (Button i in buttons)
                 {
-                    i.LeftClicked += ButtonPressed;
+                    #region LeftClickResults
+                    if (buttonLeftResults[i.ButtonNum] == 0)
+                    {
+                        i.LeftClicked += (object sender, EventArgs e) =>
+                        {
+                            string name = buttonLeftDestinations[((Button)sender).ButtonNum];
+                            background = Main.GameContent.Load<Texture2D>("Textures/help");
+                            Clear();
+                            lastMenu = menuName;
+                            LoadMenu(name);
+                        };
+                    }
+                    else if (buttonLeftResults[i.ButtonNum] == 1)
+                    {
+                        i.LeftClicked += (object sender, EventArgs e) =>
+                        {
+                            string name = buttonLeftDestinations[((Button)sender).ButtonNum];
+                            if (name != "null")
+                            {
+                                Clear();
+                                lastMenu = menuName;
+                                Console.WriteLine(string.Format("Would start level with name: {0}.", name));
+                            }
+                        };
+                    }
+                    else if (buttonLeftResults[i.ButtonNum] == 2)
+                    {
+                        i.LeftClicked += (object sender, EventArgs e) =>
+                        {
+                            Clear();
+                            lastMenu = menuName;
+                            Console.WriteLine("Would open the level designer.");
+                        };
+                    }
+                    #endregion
+
+                    #region RightClickResults
+                    if (buttonRightResults[i.ButtonNum] == 0)
+                    {
+                        i.RightClicked += (object sender, EventArgs e) =>
+                        {
+                            string name = buttonRightDestinations[((Button)sender).ButtonNum];
+                            background = Main.GameContent.Load<Texture2D>("Textures/help");
+                            Clear();
+                            lastMenu = menuName;
+                            LoadMenu(name);
+                        };
+                    }
+                    else if (buttonRightResults[i.ButtonNum] == 1)
+                    {
+                        i.RightClicked += (object sender, EventArgs e) =>
+                        {
+                            string name = buttonRightDestinations[((Button)sender).ButtonNum];
+                            if (name != "null")
+                            {
+                                Clear();
+                                lastMenu = menuName;
+                                Console.WriteLine(string.Format("Would start level with name: {0}.", name));
+                            }
+                        };
+                    }
+                    else if (buttonRightResults[i.ButtonNum] == 2)
+                    {
+                        i.RightClicked += (object sender, EventArgs e) =>
+                        {
+                            Clear();
+                            lastMenu = menuName;
+                            Console.WriteLine("Would open the level designer.");
+                        };
+                    }
+                    #endregion
                 }
             }
             catch (AssetNotFoundException e)
@@ -199,66 +293,66 @@ namespace Tower_Defense_Project
             }
             catch (FileNotFoundException e)
             {
-                ErrorHandler.RecordError(3, 001, "The menu xml file could not be found; make sure that files are named correctly and that they are correctly referenced in other menus. If this is happening at start up make sure that the Menu.xml is correctly named and present.", e.Message);
+                ErrorHandler.RecordError(3, 001, "The destination file could not be found; make sure that files are named correctly and that they are correctly referenced in other menus. If this is happening at start up make sure that the Menu.xml is correctly named and present.", e.Message);
             }
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        private void Clear()
+        {
+            buttons.Clear();
+            buttonTypes.Clear();
+            buttonLeftDestinations.Clear();
+            buttonLeftResults.Clear();
+            buttonRightDestinations.Clear();
+            buttonRightResults.Clear();
+            textures.Clear();
+            textureLocations.Clear();
+        }
+
         protected override void Update(GameTime gameTime)
         {
-            mouse = Mouse.GetState();
-            keyboard = Keyboard.GetState();
+            CurrentMouse = Mouse.GetState();
 
             try
             {
-                foreach (Button i in buttons) i.Update(CurrentMouse);
+                foreach (Button i in buttons) i.Update(Main.CurrentMouse);
             }
             catch { }
 
-            if (CurrentKeyboard.IsKeyPressed(Keys.Enter))
+            /*if (Main.CurrentKeyboard.IsKeyPressed(Keys.Enter))
             {
-                background = GameContent.Load<Texture2D>("Textures/help");
+                background = Main.GameContent.Load<Texture2D>("Textures/help");
                 buttons.Clear();
                 buttonDestinations.Clear();
                 textures.Clear();
                 textureLocations.Clear();
+                buttonResults.Clear();
                 LoadMenu("Menu");
-            }
-
-            base.Update(gameTime);
+            }*/
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            spriteBatch.Begin(SpriteSortMode.Deferred, graphics.GraphicsDevice.BlendStates.NonPremultiplied);
-
-            Window.AllowUserResizing = false;
-
             spriteBatch.Draw(background, Vector2.Zero, Color.White);
 
             for (int i = 0; i < textures.Count; i++)
             {
-                spriteBatch.Draw(textures[i], textureLocations[i], Color.White);
+                Vector2 textureSize = new Vector2((textures[i].Width / intendedResolution.X) * Main.Scale.X, (textures[i].Height / intendedResolution.Y) * Main.Scale.Y),
+                    textureLocation = textureLocations[i];
+                spriteBatch.Draw(textures[i], new RectangleF(textureLocation.X, textureLocation.Y, textureSize.X, textureSize.Y), Color.White);
             }
 
             foreach (Button i in buttons)
             {
-                spriteBatch.Draw(i.Texture, i.Position, Color.White);
+                if (buttonTypes[i.ButtonNum] == ButtonType.Rectangle)
+                {
+                    spriteBatch.Draw(i.Texture, i.Collision, Color.White);
+                }
+                else
+                {
+                    spriteBatch.Draw(i.Texture, i.Position, Color.White);
+                }
             }
-
-            spriteBatch.End();
-
-            base.Draw(gameTime);
         }
     }
 }
